c693e7e5c3d49d1feca5c233e81e1495aceaae07
Shao-Lin <baulin2005@bk.ru>
2025-01-28 15:51:39 +0300

    не запускается фронт

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build' // Замените на 'build', если хотите использовать это имя
+    outDir: 'dist'
   },
   server: {
     port: 5002,
